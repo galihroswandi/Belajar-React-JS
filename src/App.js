0f30dfb68dd5b10e4eaf90d2f3859823bc249123
galihroswandi <111326692+galihroswandi@users.noreply.github.com>
2022-09-15 09:18:03 +0700

    video10 Looping List

--- src/App.js
+++ src/App.js
@@ -3,15 +3,45 @@ import './App.css';
 import Navigation from './components/Navigation';
 import Maincontent from './components/Maincontent';
 import Footer from './components/Footer';
+import React from 'react';
 
-function App() {
-  return (
+class App extends React.Component {
+
+  constructor(props){
+    super(props);
+
+    const fruit1 = 'mangga';
+    const fruit2 = 'apple';
+    const fruit3 = 'anggur';
+  }
+  
+  render() {
+    
+    // Hygher order function react js
+
+    // {map} => memetakan array menjadi array baru "array.map( callbackFunction )"
+    const fruits = ['mangga', 'apple', 'anggur', 'jeruk', 'nanas', 'strawberry'];
+    const fruit = fruits.map( (f, index) => <li key={index}>{f}</li> );
+        // {push} => Menambahkan nilai array baru "array.push"
+
+    // {filter} => memfilter array menjadikannya array baru "array.filter( callbackFunction )"
+    const numbers = [1, 6, 7, 8, 9, 0, 2];
+    const numb = numbers.filter( (n) => n < 7)
+    const number = numb.map( (n) => <li key={n.toString()}>{n}</li>);
+
+    // {Reduce} => menggabungkan semua nilai lalu bisa menambahkan kondisi tertentu "array.reduce( (value, accumulator) => expresion )"
+    const name = ['G', 'a', 'l', 'i', 'h', 'R', 'o', 's', 'w', 'a', 'n', 'd', 'i'];
+    const fullName = name.reduce((value, acc) => value + acc);
+    const totalNilai = numb.reduce( (value, acc) => value + acc );
+
+    return (
     <div>
-      <Navigation />
-      <Maincontent />
-      <Footer />
+      <ul>
+        {number}
+      </ul>
+      <h1>Total Nilai Dari Array : {totalNilai}</h1>
     </div>
-  );
+    )
+  }
 }
-
 export default App;
